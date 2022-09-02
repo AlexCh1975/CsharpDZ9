95c3f7195cb83dac9dbd1bc4c08ac6a8117efb28
AlexCh <stovertikal@mail.ru>
2022-09-02 14:43:08 +0700

    add task 064

---
+++ task_064/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+    Задача 64: Задайте значения M и N. Напишите программу, 
+    которая выведет все натуральные числа в промежутке от M до N от большего к меньшему.
+    M = 1; N = 5. -> ""5, 4, 3, 2, 1""
+    M = 4; N = 8. -> ""8, 7, 6, 5, 4""
+*/
+
+//Console.Clear();
+
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+int n = 0, m = 0;
+
+if (numberN == numberM)
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.Write("Warning! Интервал между числами равен нулю!");
+    Console.ResetColor();
+}
+else if (numberN < numberM)
+{
+    m = numberN;
+    n = numberM;
+}
+else
+{
+    n = numberN;
+    m = numberM;
+}
+
+int[] arrayNumber = new int[n - m +1];
+
+arrayNumber = GetStringNumber(n, m, arrayNumber);
+
+PrintStringNumber(arrayNumber, numberN, numberM);
+
+
+int[] GetStringNumber(int numberN, int numberM, int[] arrayNumber, int i = 0)
+{  
+    if(numberM <= numberN) 
+    {
+        arrayNumber[i] = numberN;
+        numberN--;
+        i++;
+        return GetStringNumber(numberN, numberM, arrayNumber, i);
+    }
+     return arrayNumber;
+}
+
+void PrintStringNumber(int[] arrayNumber, int numberN, int numberM)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write($"M = {numberM}; N = {numberN} -> \" ");
+
+    for (int i = 0; i < arrayNumber.Length; i++)
+    {
+        if (arrayNumber.Length -1 == i) Console.Write($"{arrayNumber[i]} ");
+        else Console.Write($"{arrayNumber[i]}, ");
+    }
+
+    Console.WriteLine("\"");
+    Console.ResetColor();
+}
